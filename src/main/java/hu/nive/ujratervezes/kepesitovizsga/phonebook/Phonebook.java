81cfbf04d4c6c20732f9df8377734dcd2aea4247
tlev159 <t.levente1598@gmail.com>
2021-04-29 11:05:47 +0200

    Phonebook with extracted method

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -13,16 +13,21 @@ public class Phonebook {
       throw new IllegalArgumentException("Parameter must be not null!");
     }
     try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
-      for (Map.Entry<String, String> entry : contacts.entrySet()) {
-        String name = entry.getKey();
-        String phoneNumber = entry.getValue();
-        if (name == null || phoneNumber == null) {
-          throw new IllegalArgumentException("Name or phone number must be not null!");
-        }
-        writer.write(name + ": " + phoneNumber);
-      }
+      writeDatasToFile(contacts, writer);
     } catch (IOException ioe) {
       throw new IllegalArgumentException("Cannot write file!", ioe);
     }
   }
+
+  private void writeDatasToFile(Map<String, String> contacts, BufferedWriter writer) throws IOException {
+    for (Map.Entry<String, String> entry : contacts.entrySet()) {
+      String name = entry.getKey();
+      String phoneNumber = entry.getValue();
+      if (name == null || phoneNumber == null) {
+        throw new IllegalArgumentException("Name or phone number must be not null!");
+      }
+      writer.write(name + ": " + phoneNumber);
+    }
+  }
+
 }
